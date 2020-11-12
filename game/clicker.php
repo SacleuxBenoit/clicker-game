13dd433dc89bb08fb066f5cff1d53e013702a8b8
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-11-12 22:58:51 +0100

    in game/clicker.php :arrow_right: add a h1, nav with 4li in it... in style.css :arrow_right:  add list style none for the ul + add a border-top, change the height and the width for the li

--- game/clicker.php
+++ game/clicker.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
     <title>Clicker game</title>
 </head>
 <body>
-    
+    <h1>Clicker game</h1>
+
+    <nav>
+        <ul>
+            <li>Sort 1</li>
+            <li>Sort 2</li>
+            <li>Sort 3</li>
+            <li>Sort 4</li>
+        </ul>
+    </nav>
 </body>
 </html>
