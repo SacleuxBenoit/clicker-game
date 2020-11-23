49baad538b5089f4f653108269d3198b281e2d78
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-11-23 11:15:59 +0100

    add form for login + register :arrow_right: index.php... create the file login_register.css

--- index.php
+++ index.php
@@ -3,9 +3,51 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/login_register.css">
+    <title>Document</title>
 </head>
 <body>
+<header>
+    <h1>Clicker game</h1>
+</header>
 
+    <div class="login">
+        <h2>Login</h2>
+
+        <form action="#" method="post">
+            <p>
+                <label for="LoginEmail">Email :</label>
+                <input type="email" id="LoginEmail" name="LoginEmail">
+            </p>
+
+            <p>
+                <label for="LoginPass">Password :</label>
+                <input type="password" id="LoginPass" name="LoginPass">
+            </p>
+
+            <input type="submit" value="Submit">
+            
+        </form>
+    </div>
+
+    <div class="register">
+        <h2>Register</h2>
+
+        <form action="#" method="post">
+            <p>
+                <label for="RegisterEmail">Email :</label>
+                <input type="email" id="RegisterEmail" name="RegisterEmail">
+            </p>
+
+            <p>
+                <label for="RegisterPass">Password :</label>
+                <input type="password" id="RegisterPass" name="RegisterPass">
+            </p>
+
+            <input type="submit" value="Submit">
+            
+        </form>
+    </div>
 </body>
 </html>
