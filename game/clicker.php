5972558f3db10e044ce595d42e30961fc9bc927e
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-11-28 19:06:05 +0100

    add id for each button :arrow_right: clicker.php

--- game/clicker.php
+++ game/clicker.php
@@ -18,14 +18,14 @@
 
         <p class="parNav">increase damage per click : cost 15 gold <br> <button onclick="damageClick()" id="ButtonDamageClick">damage click</button></p>
 
-        <p class="parNav">small attack : cost 5 gold <br> <button onclick="smallAttack()">small attack</button></p>
+        <p class="parNav">small attack : cost 5 gold <br> <button onclick="smallAttack()" id="ButtonSmallAttack">small attack</button></p>
 
-        <p class="parNav">big attack : cost 12 gold <br> <button onclick="bigAttack()">big attack</button></p>
+        <p class="parNav">big attack : cost 12 gold <br> <button onclick="bigAttack()"  id="ButtonBigAttack">big attack</button></p>
 
-        <p class="parNav">Reduce Monster : cost 50 gold <br> <button onclick="reduceMonster()">reduce monster</button></p>
+        <p class="parNav">Reduce Monster : cost 50 gold <br> <button onclick="reduceMonster()"  id="ButtonReduceMonster">reduce monster</button></p>
 
-        <p class="parNav">Reduce Life : cost 5 gold every 2 seconds<br> <button onclick="startIntReduceLife()">reduce life</button>
-            <button onclick="clearIntReduceLife()">STOP</button>
+        <p class="parNav">Reduce Life : cost 5 gold every 2 seconds<br> <button onclick="startIntReduceLife()"  id="ButtonReduceLife">reduce life</button>
+            <button onclick="clearIntReduceLife()"  id="ButtonClearIntReduceLife">STOP</button>
         </p>
 
     </nav>
