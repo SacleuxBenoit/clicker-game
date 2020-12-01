430168033d51f2e9a2ec61e036f3300f58d275fe
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-12-01 20:56:59 +0100

    reorganization of the part nav :arrow_right: clicker.php... delete height for .parNav :arrow_right:  style.css

--- game/clicker.php
+++ game/clicker.php
@@ -16,29 +16,42 @@
     
     <nav class="navDamage">
 
-        <p class="parNav">increase damage per click : cost 15 gold <br> <button onclick="damageClick()" id="ButtonDamageClick">damage click</button>
-        <br> Click damage : +2
-        </p>
+        <div class="parNav">
+            <p>increase damage per click : cost 15 gold</p>
+            <button onclick="damageClick()" id="ButtonDamageClick">damage click</button>
+            <p>Click damage : +2</p>
+        </div>
 
-        <p class="parNav">small attack : cost 5 gold <br> <button onclick="smallAttack()" id="ButtonSmallAttack">small attack</button>
-        <br> attack damage : 25
-        </p>
+        <div class="parNav">
+            <p>small attack : cost 5 gold</p>
+            <button onclick="smallAttack()" id="ButtonSmallAttack">small attack</button>
+            <p>attack damage : 25</p>
+        </div>
 
-        <p class="parNav">big attack : cost 12 gold <br> <button onclick="bigAttack()"  id="ButtonBigAttack">big attack</button>
-        <br> attack damage : 75
-        </p>
+        <div class="parNav">
+            <p>big attack : cost 12 gold</p>
+            <button onclick="bigAttack()"  id="ButtonBigAttack">big attack</button>
+            <p>damage : 75</p>   
+        </div>
 
-        <p class="parNav">Reduce Monster : cost 50 gold <br> <button onclick="reduceMonster()"  id="ButtonReduceMonster">reduce monster</button>
-        <br> reduces the maximum number of monsters (2 monster minimum)
-        </p>
+        <div class="parNav">
+            <p>Reduce Monster : cost 50 gold</p>
+            <button onclick="reduceMonster()"  id="ButtonReduceMonster">reduce monster</button> 
+            <p> reduces the maximum number of monsters (2 monster minimum )</p>
+        </div>
 
-        <p class="parNav">Reduce Life : cost 5 gold<br> <button onclick="startIntReduceLife()"  id="ButtonReduceLife">reduce life</button>
-            <button onclick="clearIntReduceLife()"  id="ButtonClearIntReduceLife">STOP</button>
-            <br> reduce the life of monsters every 2 seconds
-        </p>
-        
-        <p class="parNav">Random gold : cost 10 gold <br> <button onclick="randomGold()"  id="ButtonRandomGold">random gold</button>
-        <br> give you between 7 and 15 gold </p>
+        <div class="parNav">
+            <p>Reduce Life : cost 5</p>
+                <button onclick="startIntReduceLife()"  id="ButtonReduceLife">reduce life</button>
+                <button onclick="clearIntReduceLife()"  id="ButtonClearIntReduceLife">STOP</button>
+                <p> reduce the life of monsters every 2 seconds </p>
+        </div>
+
+        <div class="parNav">
+            <p>Random gold : cost 10 gold</p>
+            <button onclick="randomGold()"  id="ButtonRandomGold">random gold</button>
+            <p> give you between 7 and 15 gold </p>
+        </div>
 
     </nav>
 
