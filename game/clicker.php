5426c53e62ae30644bd52f59d10833e072c1b724
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-12-03 23:58:38 +0100

    add id for every descriptions :arrow_right: clicker.php

--- game/clicker.php
+++ game/clicker.php
@@ -25,38 +25,38 @@
         <div class="parNav">
             <p>small attack : cost 5 gold</p>
             <button onclick="smallAttack()" id="ButtonSmallAttack">small attack</button>
-            <p>attack damage : 25</p>
+            <p id="descSmallAttack">Attack damage : 25</p>
         </div>
 
         <div class="parNav">
             <p>big attack : cost 12 gold</p>
             <button onclick="bigAttack()"  id="ButtonBigAttack">big attack</button>
-            <p>damage : 75</p>   
+            <p id="descBigAttack">Attack damage : 75</p>   
         </div>
 
         <div class="parNav">
             <p>Reduce Monster : cost 50 gold</p>
             <button onclick="reduceMonster()"  id="ButtonReduceMonster">reduce monster</button> 
-            <p> reduces the maximum number of monsters (2 monster minimum )</p>
+            <p id="descReduceMonster"> Reduces the maximum number of monsters (2 monster minimum )</p>
         </div>
 
         <div class="parNav">
             <p>Reduce Life : cost 5 gold every 2 second</p>
                 <button onclick="startIntReduceLife()"  id="ButtonReduceLife">reduce life</button>
                 <button onclick="clearIntReduceLife()"  id="ButtonClearIntReduceLife">STOP</button>
-                <p> reduce the life of monsters every 2 second </p>
+                <p id="DescReduceLife"> Reduce the life of monsters every 2 second </p>
         </div>
 
         <div class="parNav">
             <p>Random gold : cost 10 gold</p>
             <button onclick="randomGold()"  id="ButtonRandomGold">random gold</button>
-            <p> give you between 7 and 15 gold </p>
+            <p id="descRandomGold"> Give you between 7 and 15 gold </p>
         </div>
         
         <div class="parNav">
             <p>Delete life : cost 100 gold</p>
             <button onclick="deleteLife()"  id="ButtonDeleteLife">Delete life</button>
-            <p> Reduce the maximum HP by 10 </p>
+            <p id="descDeleteLife"> Reduce the maximum HP by 10 </p>
         </div>
 
     </nav>
