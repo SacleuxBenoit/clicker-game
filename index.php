32bb855e403400f14c325a240443060f7a367a02
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-26 13:28:14 +0100

    change link for the files script.js && style.css due to the move :arrow_right: index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="./style/style.css">
     <title>Clicker game</title>
 </head>
 <body>
@@ -90,6 +90,6 @@
 
     </div>
 
-    <script src="../javascript/script.js"></script>
+    <script src="./javascript/script.js"></script>
 </body>
 </html>
